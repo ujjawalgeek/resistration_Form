d17b3a7752c689fd0ded25d423ccdcb117c12c2a
ujjawalgeek <chauhanujjawal070@gmail.com>
2026-02-14 00:06:36 +0530

    Updated backend structure

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+// Load environment variables
+dotenv.config({
+  path: "./.env",
+});
+
+// Connect DB → then start server
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, () => {
+      console.log(` Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" MongoDB connection failed !!!", err);
+  });
